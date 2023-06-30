fefca5540341b7d8f1a976e20e95057400ae09d6
Nit <66027553+Nitlix@users.noreply.github.com>
2023-07-01 00:58:00 +0300

    0.2.1 Quickfix
    Changes:
    
    ! Renamed sapsar/active/activeRender to sapsar/active/ActiveRender
    
    Might be small, but the aesthetics say it all ☠️

--- src/active/activeRender.js
+++ src/active/activeRender.js
@@ -1,13 +1,13 @@
 const { addProcess } = require("../util/ActiveBuild")
 
-function activeRender(func, selector, args, build){
+function ActiveRender(func, selector, args, build){
     addProcess(
         build,
         async ()=>{
             const renderedContent = await func(args)
-            return "<script data-sapsar-activeRender>content=`"+renderedContent+"`;selector='"+selector+"';try{'loading'!=document.readyState?document.querySelector(selector).innerHTML=content:document.addEventListener('DOMContentLoaded',document.querySelector(selector).innerHTML=content)}catch(e){}</script>"
+            return "<script data-sapsar-ActiveRender>content=`"+renderedContent+"`;selector='"+selector+"';try{'loading'!=document.readyState?document.querySelector(selector).innerHTML=content:document.addEventListener('DOMContentLoaded',document.querySelector(selector).innerHTML=content)}catch(e){}</script>"
         }
     )
 }
 
-module.exports = activeRender;
+module.exports = ActiveRender;
