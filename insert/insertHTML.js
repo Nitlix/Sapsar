e55d762958657d08351087cbdece45f7ee2a37b5
Nit <66027553+Nitlix@users.noreply.github.com>
2023-07-26 20:51:21 +0300

    0.3.8 Alpha X2

--- insert/insertHTML.js
+++ insert/insertHTML.js
@@ -1,5 +1,10 @@
 const { getHTML } = require("../util/SapsarCompiler");
 
+/**
+ * @param {function} component The HTML component name to insert.
+ * @returns {string} The HTML of the component.
+ * @description Your HTML is imported and stored for production (mostly from markdowns), and can be inserted into the DOM using this function.
+ */
 function insertHTML(component){
     return getHTML(component);
 }
